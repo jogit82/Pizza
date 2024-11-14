54f06848ea1f0c9ce075892ed1a63e9f072a92d1
Jo <jo@Jos-iMac.kitchenpc.net>
2024-11-13 20:13:24 -0800

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "@tanstack/react-router";
+import { use } from "react";
+import { CartContext } from "./contexts";
+
+export default function Header() {
+  const [cart] = use(CartContext);
+  return (
+    <nav>
+      <Link to={"/"}>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+      </Link>
+      <div className="nav-cart">
+        🛒<span className="nav-cart-number">{cart.length}</span>
+      </div>
+    </nav>
+  );
+}
